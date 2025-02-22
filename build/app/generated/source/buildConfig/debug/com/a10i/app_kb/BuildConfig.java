1d7bfe32adf3723b7cf9bb0de5241b055c54335a
rybinski92 <rybinski92@gmail.com>
2025-02-22 07:23:53 +0100

    Aktualizacja plików

--- build/app/generated/source/buildConfig/debug/com/a10i/app_kb/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/a10i/app_kb/BuildConfig.java
@@ -10,11 +10,11 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0.0";
   // Field from build type: debug
-  public static final String API_KEY1 = "V2-WuGVt-d2uuu-w7xds-l31vv-pU3hO-DNrxG-QB78D-WLD4g";
+  public static final String API_KEY1 = "";
   // Field from build type: debug
-  public static final String API_KEY2 = "V2-ZLb8Y-JzyV1-eUXjV-46Nbq-oFpXU-jr8IP-Eozsk-7aEOK";
+  public static final String API_KEY2 = "";
   // Field from build type: debug
-  public static final String API_KEY3 = "V2-F5h9i-H4xyN-PNNOf-rmtPX-lwZLk-W1tb3-H8B48-oBMzN";
+  public static final String API_KEY3 = "";
   // Field from build type: debug
-  public static final String API_KEY4 = "V2-badec-FfQpV-P3ZJv-kS09f-HKwx0-HTPfn-XQqor-RSYq6";
+  public static final String API_KEY4 = "";
 }
