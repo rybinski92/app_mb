966bd87d141d131d44bd33f8c4b5e00d05109b3e
rybinski92 <rybinski92@gmail.com>
2025-04-07 21:10:20 +0200

    Zaktualizowana wersja aplikacji

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "840293309"
+    serviceWorkerVersion: "3330421994"
   }
 });
--- lib/main.dart
+++ lib/main.dart
@@ -50,14 +50,28 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   List<Map<String, String>> polecaneZawody = [];
-  // bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     _loadLocalExcelData();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // Ten fragment wykona się za każdym razem, gdy aplikacja wraca na ekran
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _loadLocalExcelData(); // automatyczne odświeżenie
+    }
   }
 
   Future<void> _loadLocalExcelData() async {
